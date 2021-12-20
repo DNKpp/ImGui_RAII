1b126a9832c28a0d307ec23214765e42f599fc96
DNKpp <DNKpp2011@gmail.com>
2021-12-20 18:13:31 +0100

    add missing license text
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- src/imgui_raii/imgui_raii.hpp
+++ src/imgui_raii/imgui_raii.hpp
@@ -1,5 +1,12 @@
+//          Copyright Dominic Koepke 2021 - 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef IMGUI_RAII_IMGUI_RAII_HPP
 #define IMGUI_RAII_IMGUI_RAII_HPP
+
+#pragma once
 
 #include <concepts>
 #include <functional>
