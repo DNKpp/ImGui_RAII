29ad2c73b3676d35e83fef4ff9fd2cbd1bc3311b
DNKpp <DNKpp2011@gmail.com>
2021-12-20 17:59:06 +0100

    add example setup_with_imgui
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ examples/setup_with_imgui/main.cpp
@@ -0,0 +1,35 @@
+#include <imgui_raii/imgui_raii.hpp>
+
+imgui_raii::BeginContext setup_imgui_context()
+{
+	imgui_raii::BeginContext context{};
+
+	ImGuiIO& io = ImGui::GetIO();
+
+	// Build atlas
+	unsigned char* tex_pixels = nullptr;
+	int tex_w, tex_h;
+	io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
+
+	io.DisplaySize = ImVec2(1920, 1080);
+	io.DeltaTime = 1.0f / 60.0f;
+
+	return context;
+}
+
+int main()
+{
+	auto context = setup_imgui_context();
+
+	imgui_raii::NewFrame{} /
+		[]
+		{
+			imgui_raii::Begin{ "Hello, World!" } /
+				[]
+				{
+					if (ImGui::Button("Test Button"))
+					{
+					}
+				};
+		};
+}
